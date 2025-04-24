68d1214b62e6cf9b7e73bb715e62dc5281657376
taehyeon <max5915@naver.com>
2025-04-24 20:02:36 +0900

    IndexOutOfBoundsException 오류 발생

--- src/kioskLv4/Select.java
+++ src/kioskLv4/Select.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Select extends Menu{
     Scanner scanner = new Scanner(System.in);
     int num1 = 0;
+
+
     void selectFood() {
         int i=0;
         System.out.print("메뉴를 선택해주세요 : ");
         num1 = scanner.nextInt();
-        if (num1 == menuItems.size()) {
-            for (MenuItem food : menuItems) {
-                i++;
-                System.out.println(i+". "+food.name + " | W " + food.price + " | " + food.explanation);
+        for(int j=0; j<3; j++) {
+            i++;
+            if (num1 == i) {
+                System.out.println(i + ". "+ menuItems.get(j).name + " | W " + menuItems.get(j).price + " | " + menuItems.get(j).explanation);
             }
         }
+
+
     }
 }
